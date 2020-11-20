54c43f2f8913297a1358c04548a38d32d7f312d6
RasmusGJ <60926479+RasmusGJ@users.noreply.github.com>
2020-11-20 10:27:19 +0100

    asldkjasd'

---
+++ surf spotter dot net core/surf spotter dot net core/Models/Middlewares/ConverterMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace surf_spotter_dot_net_core.Models.Middlewares
+{
+    public class ConverterMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ConverterMiddleware(RequestDelegate next, ILoggerFactory logFactory)
+        {
+            _next = next;
+            _logger = logFactory.CreateLogger("MyMiddleware");
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            
+        }
+    }
+    public static class MyMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ConverterMiddleware>();
+        }
+    }
+}
+

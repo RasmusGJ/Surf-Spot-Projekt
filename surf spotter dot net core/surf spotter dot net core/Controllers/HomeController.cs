1fb408cc6a0908c4226b1d12f4459cbb8d825024
RasmusGJ <60926479+RasmusGJ@users.noreply.github.com>
2020-11-24 10:01:56 +0100

    ryddet op bbby

--- surf spotter dot net core/surf spotter dot net core/Controllers/HomeController.cs
+++ surf spotter dot net core/surf spotter dot net core/Controllers/HomeController.cs
@@ -57,72 +57,7 @@ namespace surf_spotter_dot_net_core.Controllers
             return View();
         }
 
-        [Route("CreateSpot")]
-        [Route("Home/CreateSpot")]
-        [Route("CS")]
-        [HttpGet]
-        public IActionResult CreateSpot()
-        {
-            return View();
-        }
-
-        [HttpPost, Route("CreateSpot")]
-        [HttpPost, Route("Home/CreateSpot")]
-        [HttpPost, Route("CS")]
-        public IActionResult CreateSpot([Bind("Name, Lat, Lng, SpotStatus")] Spot spot)
-        {
-            if (!ModelState.IsValid)
-                return View();
-
-            _db.Spots.Add(spot);
-            _db.SaveChanges();
-            return View();
-        }
-
-        [HttpGet, Route("spots")]
-        [HttpGet, Route("Home/spots")]
-        [HttpGet, Route("s")]
-        public async Task<ActionResult<SpotsViewModel>> Spots()
-        {
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
-            SpotsViewModel spotsViewModel = new SpotsViewModel();
-
-            var spot = _client.GetOneSpot(2);
-            var spots = await _client.GetAllSpots();
-            spotsViewModel.Spots = spots;
-
-            //var hourly = await _client.GetAllByHourly(spot.Result.Lat, spot.Result.Lng);
-            //spotsViewModel.Hourly = hourly;
-
-            var daily = await _client.GetAllByDaily(spot.Result.Lat, spot.Result.Lng);
-            spotsViewModel.Daily = daily;
-
-            return View(spotsViewModel);
-        }
-
-
-        [HttpPost, Route("s")]
-        [HttpPost, Route("spots")]
-        [HttpPost, Route("Home/spots")]
-        public async Task<ActionResult> Spots(SpotsViewModel spotsViewModel)
-        {
-            var spots = await _client.GetAllSpots();
-            spotsViewModel.Spots = spots;
-            foreach (Spot s in spotsViewModel.Spots)
-            {
-                if(s.Id == spotsViewModel.SpotId)
-                {
-                    var daily = await _client.GetAllByDaily(s.Lat, s.Lng);
-                    spotsViewModel.Daily = daily;
-                    break;
-                }                               
-            }
-           
-            return View(spotsViewModel);
-        }
+       
 
         [Route("Privacy")]
         [Route("home/Privacy")]
@@ -131,33 +66,6 @@ namespace surf_spotter_dot_net_core.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        [Route("showspots")]
-        [HttpGet]
-        public async Task<ActionResult> ShowSpots()
-        {
-
-            var spots = await _client.GetAllSpots();
-
-            return View(spots);
-        }
-
-        [Route("editspot")]
-        [Route("Home/editspot")]
-        [HttpGet]
-        public async Task<ActionResult> EditSpot()
-        {
-            var spot = await _client.GetOneSpot(1);
-            return View(spot);
-        }
-
-        [Route("editspot")]
-        [Route("Home/editspot")]
-        [HttpPost]
-        public IActionResult EditSpot(Spot spot)
-        {
-            return View();
-        }
+        }        
     }
 }
---
+++ surf spotter dot net core/surf spotter dot net core/Controllers/SpotController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using surf_spotter_dot_net_core.Models;
+using surf_spotter_dot_net_core.ViewModels;
+
+namespace surf_spotter_dot_net_core.Controllers
+{
+    public class SpotController : Controller
+    {
+        private readonly ILogger<SpotController> _logger;
+        private readonly IdentityDataContext _db;
+        private readonly HttpProxy _client;
+
+        public SpotController(ILogger<SpotController> logger, IdentityDataContext db, HttpProxy client)
+        {
+            _logger = logger;
+            _db = db;
+            _client = client;
+        }
+
+        [HttpGet, Route("spots")]
+        [HttpGet, Route("s")]
+        [HttpGet, Route("")]
+        public async Task<ActionResult<SpotsViewModel>> Spots()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            SpotsViewModel spotsViewModel = new SpotsViewModel();
+
+            var spot = _client.GetOneSpot(2);
+            var spots = await _client.GetAllSpots();
+            spotsViewModel.Spots = spots;
+
+            //var hourly = await _client.GetAllByHourly(spot.Result.Lat, spot.Result.Lng);
+            //spotsViewModel.Hourly = hourly;
+
+            var daily = await _client.GetAllByDaily(spot.Result.Lat, spot.Result.Lng);
+            spotsViewModel.Daily = daily;
+
+            return View(spotsViewModel);
+        }
+
+        [HttpPost, Route("spots")]
+        [HttpPost, Route("s")]
+        [HttpPost, Route("")]
+        public async Task<ActionResult> Spots(SpotsViewModel spotsViewModel)
+        {
+            var spots = await _client.GetAllSpots();
+            spotsViewModel.Spots = spots;
+            foreach (Spot s in spotsViewModel.Spots)
+            {
+                if (s.Id == spotsViewModel.SpotId)
+                {
+                    var daily = await _client.GetAllByDaily(s.Lat, s.Lng);
+                    spotsViewModel.Daily = daily;
+                    break;
+                }
+            }
+
+            return View(spotsViewModel);
+        }
+
+        [Route("CreateSpot")]
+        [Route("CS")]
+        [Route("")]
+        [HttpGet]
+        public IActionResult CreateSpot()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("CreateSpot")]
+        [HttpPost, Route("CS")]
+        [HttpPost, Route("")]
+        public IActionResult CreateSpot([Bind("Name, Lat, Lng, SpotStatus")] Spot spot)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            _db.Spots.Add(spot);
+            _db.SaveChanges();
+            return View();
+        }
+
+        [Route("showspots")]
+        [HttpGet]
+        public async Task<ActionResult> ShowSpots()
+        {
+
+            var spots = await _client.GetAllSpots();
+
+            return View(spots);
+        }
+
+        [Route("editspot")]
+        [Route("Home/editspot")]
+        [HttpGet]
+        public async Task<ActionResult> EditSpot()
+        {
+            var spot = await _client.GetOneSpot(1);
+            return View(spot);
+        }
+
+        [Route("editspot")]
+        [Route("Home/editspot")]
+        [HttpPost]
+        public IActionResult EditSpot(Spot spot)
+        {
+            return View();
+        }
+    }
+}
